e81226371681a3bd9188de4075160d9d76b3b137
mulherinryan <mulherinryan@gmail.com>
2015-05-01 01:16:57 -0400

    Header File Addition
    Adding math

--- Grade_Calc/Grade_Calc/main.cpp
+++ Grade_Calc/Grade_Calc/main.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  Grade_Calc
-//
-//  Created by Ryan Mulherin on 5/1/15.
-//  Copyright (c) 2015 Ryan Mulherin. All rights reserved.
-//
-
 #include <iostream>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+
     return 0;
 }

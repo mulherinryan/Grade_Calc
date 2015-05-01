fe8155fe584bd101a9ec123f895b932bb0267a1b
mulherinryan <mulherinryan@gmail.com>
2015-05-01 01:10:38 -0400

    First Save
    backup for first save

---
+++ Grade_Calc/Grade_Calc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Grade_Calc
+//
+//  Created by Ryan Mulherin on 5/1/15.
+//  Copyright (c) 2015 Ryan Mulherin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
